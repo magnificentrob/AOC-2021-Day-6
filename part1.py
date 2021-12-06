5351c4dce597ea3cd99ec2af60fb5bc14ed8d487
magnificentrob <31416219+magnificentrob@users.noreply.github.com>
2021-12-06 18:37:09 -0500

    Update part1.py

--- part1.py
+++ part1.py
@@ -1,16 +1,18 @@
 import csv
 lanternfish = []
-with open ('input.txt', 'r') as file:
+with open ('test.txt', 'r') as file:
 	reader = csv.reader(file)
 	for row in reader:
 		for i in row:
 			lanternfish.append(int(i))
 
-for x in range(0,81):
+numoffish = (len(lanternfish))
+
+for x in range(0,80):
 	for i in range(len(lanternfish)):
-		if lanternfish[i] == -1:
+		if lanternfish[i] == 0:
 			lanternfish[i] = 6
 			lanternfish.append(8)
-	for i in range(len(lanternfish)):
-		lanternfish[i]-= 1
-print(len(lanternfish))
+		else:
+			lanternfish[i] -= 1
+	print(len(lanternfish))
